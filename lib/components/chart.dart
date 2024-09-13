85cdc65e3ca665736603748fd696f9b7d5ef70e2
Ana Troiano <anac.troiano@gmail.com>
2024-09-13 15:12:03 -0300

    Projeto finalizado

---
+++ lib/components/chart.dart
@@ -0,0 +1,62 @@
+import 'package:expenses/components/chart_bar.dart';
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  const Chart(this.recentTransactions, {super.key});
+
+  List<Map<String, Object>> get groupedTransactions {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+
+      double totalSum = 0.0;
+
+      for (var i = 0; i < recentTransactions.length; i++) {
+        bool sameDay = weekDay.day == recentTransactions[i].date.day;
+        bool sameMonth = weekDay.month == recentTransactions[i].date.month;
+        bool sameYear = weekDay.year == recentTransactions[i].date.year;
+
+        if (sameDay && sameMonth && sameYear) {
+          totalSum += recentTransactions[i].value;
+        }
+      }
+
+      return {
+        'day': DateFormat.E().format(weekDay)[0],
+        'value': totalSum,
+      };
+    }).reversed.toList();
+  }
+
+  double get _weekTotalValue {
+    return groupedTransactions.fold(0.0, (sum, tr) {
+      return sum + (tr['value'] as double);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        elevation: 6,
+        margin: const EdgeInsets.all(20),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: groupedTransactions.map((tr) {
+              return Flexible(
+                fit: FlexFit.tight,
+                child: ChartBar(
+                  tr['day'].toString(),
+                  tr['value'] as double,
+                  _weekTotalValue == 0 ? 0 : ((tr['value'] as double) / _weekTotalValue),
+                ),
+              );
+            }).toList(),
+          ),
+        ));
+  }
+}
